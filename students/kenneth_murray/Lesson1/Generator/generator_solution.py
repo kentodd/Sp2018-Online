17628c4a7ddb6a590e06d8ac45c12625aad1dad2
Ken <kentodd@frontier.com>
2018-04-13 03:58:59 -0700

    renameed file to work with gen test

---
+++ students/kenneth_murray/Lesson1/Generator/generator_solution.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+'''Generators for lesson 1'''
+
+
+def intsum():
+    import math
+    sum([x*x for x in range(10)])
+
+def doubler():
+    import math
+
+
+def fib():
+    import math
+
+
+def prime():
+    import math
+
+
+if __name__ == "__main__":
