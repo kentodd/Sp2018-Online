faf528cdab5b6c184e71b396d10d1449146dc7e4
Ken <kentodd@frontier.com>
2018-04-15 22:10:17 -0700

    Generate Eds songs complete

--- students/kenneth_murray/lesson2/music_generator/generate_favorite_music.py
+++ students/kenneth_murray/lesson2/music_generator/generate_favorite_music.py
@@ -4,16 +4,15 @@ Generate a favorite artist list of songs
 '''
 
 
-def generate_my_music(artist,csv_in = 'c:\\test\\featuresdf.csv'):
+def generate_my_music(artist, csv_in='c:\\test\\featuresdf.csv'):
     import pandas as pd
     music = pd.read_csv(csv_in)
-    music_zip = zip(music.artists,music.name)
-    
-    music_list = sorted([track for track in music_zip if track[0] == artist],key=lambda track_name: track_name[1])
-    print(music_list)
+    music_zip = zip(music.artists, music.name)
+    while True:
+        music_list = sorted([track for track in music_zip if track[0] == artist], key=lambda track_name: track_name[1])
+        yield music_list
 
 
 if __name__ == '__main__':
-
-    generate_my_music(artist="Ed Sheeran")
-
+    Ed = generate_my_music(artist="Ed Sheeran")
+    print(next(Ed))
